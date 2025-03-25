f3ec0fa956dc195639c83d4bb8c36999981f0cfc
Simone Andrea Muscas <simoneandrea.muscas@gmail.com>
2025-03-25 05:51:21 +0100

    Fix Spring Boot security configuration
      TBA1

---
+++ src/main/java/com/niolikon/taskboard/authenticator/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.niolikon.taskboard.authenticator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/Auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
